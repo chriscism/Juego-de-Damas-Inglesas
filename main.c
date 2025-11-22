079ec1eb67e3573faed3a86731c8dc25cfaa6534
Cristhian <morales.666@proton.me>
2025-11-21 18:33:47 -0800

    Agregué funciones para el dibujado del tablero

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 
 #define RED "\033[0;31m"
 #define BLUE "\033[0;34m"
@@ -17,6 +18,91 @@ typedef struct casilla {
   ficha_t ficha;
 } casilla_t;
     
+void imprimir_tablero(casilla_t tablero[8][8]);
+void inicializar_tablero(casilla_t tablero[8][8]);
 int main(){
   
+}
+
+void inicializar_tablero(casilla_t tablero[8][8]) {
+  ficha_t ficha_jugador1 = {RED};
+  ficha_t ficha_jugador2 = {BLUE};
+  casilla_t casilla_aux;
+  /*La impresion se realizara considerando que se esta viendo el tablero desde
+   *arriba, comenzando con la esquina superior izquierda, la cual se
+   *considerara como la posicion 0,0.
+   */
+  for (int i = 0; i < 8; i++) {
+    for (int j = 0; j < 8; j++) {
+      // Si es fila par o 0 comienza con casilla clara (pares o 0 son claras)
+      if (i % 2 == 0) {
+        if (j % 2 == 0) {
+          casilla_aux.ocupada = false;
+          strcpy(casilla_aux.color, WHITE);
+        } else {
+          // Fichas del primer jugador
+          if (i == 0 || i == 2) {
+            casilla_aux.ocupada = true;
+            casilla_aux.ficha = ficha_jugador1;
+          } else if (i == 6) { // Fichas del segundo jugador
+            casilla_aux.ocupada = true;
+            casilla_aux.ficha = ficha_jugador2;
+          } else {
+            casilla_aux.ocupada = false;
+          }
+          strcpy(casilla_aux.color, YELLOW);
+        }
+      } else {
+        // Si es fila impar comienza con casilla oscura (pares o 0 son oscuras)
+        if (j % 2 == 0) {
+          if (i == 1) {
+            casilla_aux.ocupada = true;
+            casilla_aux.ficha = ficha_jugador1;
+          } else if (i == 5 || i == 7) {
+            casilla_aux.ocupada = true;
+            casilla_aux.ficha = ficha_jugador2;
+          } else {
+            casilla_aux.ocupada = false;
+          }
+          strcpy(casilla_aux.color, YELLOW);
+        } else {
+          casilla_aux.ocupada = false;
+          strcpy(casilla_aux.color, WHITE);
+        }
+      }
+      tablero[i][j] = casilla_aux;
+    }
+  }
+  imprimir_tablero(tablero);
+}
+
+void imprimir_tablero(casilla_t tablero[8][8]) {
+  // Cambiar al color correspondiente
+  for (int j = 0; j < 8; j++) {
+    printf("    %d  ", j);
+  }
+  for (int i = 0; i < 8; i++) {
+    printf("\n");
+    for (int j = 0; j < 8; j++) {
+      printf("%s", tablero[i][j].color);
+      printf("   --- ");
+    }
+    printf("\n");
+    printf("%s", WHITE);
+    printf("%d", i);
+    for (int j = 0; j < 8; j++) {
+      printf("%s", tablero[i][j].color);
+      if (tablero[i][j].ocupada)
+        printf(" | %s%c %s| ", tablero[i][j].ficha.color, 'P',
+               tablero[i][j].color);
+      else
+        printf(" |   | ");
+    }
+    printf("\n");
+    for (int j = 0; j < 8; j++) {
+      printf("%s", tablero[i][j].color);
+      printf("   --- ");
+    }
+    printf("\n");
+  }
 }
